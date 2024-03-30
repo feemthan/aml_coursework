1365a9c87af22524402514ac8fc403331407ed2a
feemthan <mohamedfaheemthanveer@gmail.com>
2024-03-30 13:09:59 +0000

    use label encoder

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,7 @@ import subprocess
 import pdb
 import pandas as pd
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
 
 # call ffmpeg to generate videos via subprocess
 root_dir = 'HMDB_simp'
@@ -27,20 +28,25 @@ current_dir = os.getcwd()
 class_path = []
 for dir_path, _, files in os.walk(root_dir):
     for file in files:
-        paths = os.path.join(current_dir, dir_path, file)
-        class_name = dir_path.split('/')[-1]
-        class_path.append([paths, class_name])
+        if not file.endswith('.csv'):
+            paths = os.path.join(current_dir, dir_path, file)
+            class_name = dir_path.split('/')[-1]
+            class_path.append([f"{paths} {class_name}"])
 
 # convert to dataframe to be run by train_test_split
 dataset = pd.DataFrame(class_path)
 dataset.to_csv('class_path.csv', index=False, header=False)
 
-file_path = dataset.iloc[:,0]
-class_name = dataset.iloc[:, 1]
+file_path = dataset.iloc[:, 0].str.split(' ', expand=True)[0]
+class_name = dataset.iloc[:, 0].str.split(' ', expand=True)[1]
 
-# split multiple times for 70 15 15
-X_train, X_temp, y_train, y_temp = train_test_split(file_path, class_name, test_size=0.3, random_state=2538)
-X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=2538)
+label_encoder = LabelEncoder()
+class_name = label_encoder.fit_transform(class_name)
+
+# split multiple times for 70 15 15, put your student ID here
+STUDENT_ID = 2538
+X_train, X_temp, y_train, y_temp = train_test_split(file_path, class_name, test_size=0.3, random_state=STUDENT_ID)
+X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=0.5, random_state=STUDENT_ID)
 
 # convert to dataframe
 train_df = pd.DataFrame({'filepath': X_train, 'class': y_train})
@@ -48,6 +54,6 @@ val_df = pd.DataFrame({'filepath': X_val, 'class': y_val})
 test_df = pd.DataFrame({'filepath': X_test, 'class': y_test})
 
 # save to csv without index or header
-train_df.to_csv('train.csv', index=False, header=False)
-val_df.to_csv('val.csv', index=False, header=False)
-test_df.to_csv('test.csv', index=False, header=False)
+train_df.to_csv('video_outputs/train.csv', index=False, header=False, sep=' ')
+val_df.to_csv('video_outputs/val.csv', index=False, header=False, sep=' ')
+test_df.to_csv('video_outputs/test.csv', index=False, header=False, sep=' ')
